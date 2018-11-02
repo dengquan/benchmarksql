e247e4a9571d97460d5cdbcd86ac011679dd3994
dengquan <dengquan@ennnew.cn>
2018-11-02 23:47:06 +0800

    add queryTimeoutSec in loaddata

--- src/LoadData/LoadData.java
+++ src/LoadData/LoadData.java
@@ -50,6 +50,7 @@ public class LoadData implements jTPCCConfig {
   private static boolean            outputFiles   = false;
   private static PrintWriter        out           = null;
   private static long               lastTimeMS    = 0;
+  private static int                queryTimeoutSec = 0;
 
 
   public static void main(String[] args) {
@@ -136,6 +137,7 @@ public class LoadData implements jTPCCConfig {
         try {
             Properties ini = new Properties();
             ini.load( new FileInputStream(System.getProperty("prop")));
+            queryTimeoutSec = Integer.parseInt(ini.getProperty("queryTimeoutSec","0"));
             numWarehouses = Integer.parseInt(ini.getProperty("warehouses",Integer.toString(configWhseCount)));
             commitCount = Integer.parseInt(ini.getProperty("commitCount",Integer.toString(configCommitCount)));
         } catch (IOException e) {
@@ -204,11 +206,13 @@ static void initJDBC() {
       ("INSERT INTO " + keyspace + ".district " +
        " (d_id, d_w_id, d_ytd, d_tax, d_next_o_id, d_name, d_street_1, d_street_2, d_city, d_state, d_zip) " +
        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    distPrepStmt.setQueryTimeout(queryTimeoutSec);
 
     itemPrepStmt = conn.prepareStatement
       ("INSERT INTO " + keyspace + ".item " +
        " (i_id, i_name, i_price, i_data, i_im_id) " +
        "VALUES (?, ?, ?, ?, ?)");
+    itemPrepStmt.setQueryTimeout(queryTimeoutSec);
 
     custPrepStmt = conn.prepareStatement
       ("INSERT INTO " + keyspace + ".customer " +
@@ -218,6 +222,7 @@ static void initJDBC() {
          "c_street_1, c_street_2, c_city, c_state, c_zip, " +
          "c_phone, c_since, c_middle, c_data) " +
        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    custPrepStmt.setQueryTimeout(queryTimeoutSec);
 
     histPrepStmt = conn.prepareStatement
       ("INSERT INTO " + keyspace + ".history " +
@@ -225,12 +230,14 @@ static void initJDBC() {
          "h_d_id, h_w_id, " +
          "h_date, h_amount, h_data) " +
        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    histPrepStmt.setQueryTimeout(queryTimeoutSec);
 
     ordrPrepStmt = conn.prepareStatement
       ("INSERT INTO " + keyspace + ".oorder " +
        " (o_id, o_w_id,  o_d_id, o_c_id, " +
          "o_carrier_id, o_ol_cnt, o_all_local, o_entry_d) " +
        "VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    ordrPrepStmt.setQueryTimeout(queryTimeoutSec);
 
     orlnPrepStmt = conn.prepareStatement
       ("INSERT INTO " + keyspace + ".order_line " +
@@ -238,11 +245,13 @@ static void initJDBC() {
          "ol_number, ol_i_id, ol_delivery_d, " +
          "ol_amount, ol_supply_w_id, ol_quantity, ol_dist_info) " +
        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    orlnPrepStmt.setQueryTimeout(queryTimeoutSec);
 
     nworPrepStmt = conn.prepareStatement
       ("INSERT INTO " + keyspace + ".new_order " +
        " (no_w_id, no_d_id, no_o_id) " +
        "VALUES (?, ?, ?)");
+    nworPrepStmt.setQueryTimeout(queryTimeoutSec);
 
     stckPrepStmt = conn.prepareStatement
       ("INSERT INTO " + keyspace + ".stock " +
@@ -250,11 +259,13 @@ static void initJDBC() {
          "s_dist_01, s_dist_02, s_dist_03, s_dist_04, s_dist_05, " +
          "s_dist_06, s_dist_07, s_dist_08, s_dist_09, s_dist_10) " +
        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    stckPrepStmt.setQueryTimeout(queryTimeoutSec);
 
     whsePrepStmt = conn.prepareStatement
        ("INSERT INTO " + keyspace + ".warehouse " +
         " (w_id, w_ytd, w_tax, w_name, w_street_1, w_street_2, w_city, w_state, w_zip) " +
         "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    whsePrepStmt.setQueryTimeout(queryTimeoutSec);
 
   } catch(SQLException se) {
     System.out.println(se.getMessage());
